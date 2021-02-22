ec26a27c267e3127fd5cd953e788a5c7e81ece57
amitby98 <77382577+amitby98@users.noreply.github.com>
2021-02-22 13:26:32 +0200

    solved question 1

--- 01-making-promises/answers.js
+++ 01-making-promises/answers.js
@@ -1,48 +1,37 @@
-
 /**
- * 
+ *
  * EXERCISE 1
- * 
+ *
  * @returns {Promise<3>}
  */
-function makePromiseResolveWith3(){
-  /* IMPLEMENT ME! */
+function makePromiseResolveWith3() {
+  return Promise.resolve(3);
 }
 
 /**
- * 
+ *
  * EXERCISE 2
- * 
+ *
  * @returns {Promise<,"Boo!">}
  */
-function makePromiseRejectWithBoo(){
-  /* IMPLEMENT ME! */
-}
 
 /**
- * 
+ *
  * EXERCISE 3
- * 
+ *
  * @param {boolean} itShouldResolve - Whether the promise should resolve or reject
  * @returns {Promise<undefined, undefined>}
  */
 
-function makePromiseWithConstructor(itShouldResolve){
-  return new Promise((resolve, reject) => {
-    /* If itShouldResolve is true, call resolve */
-    /* If itShouldResolve is false, call reject */
-  });
-}
-
 /**
- * 
+ *
  * EXERCISE 4
  *
- * @param {any} value 
- * @param {number} delayInMs 
+ * @param {any} value
+ * @param {number} delayInMs
  * @return {Promise<any>} - A promise that will resolve with the value after delayInMs milliseconds
  */
-function makeDelayPromise(value, delayInMs){
+function makeDelayPromise(value, delayInMs) {
   /* Return a promise that resolves with the value after delayInMs */
 }
 
